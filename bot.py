5f4d449100275a9f50143fd638645d5c1ab58ee1
Chirag Rana <chirag.rana@mail.utoronto.ca>
2020-07-26 23:44:28 -0400

    added other functions from scrapper

--- bot.py
+++ bot.py
@@ -23,8 +23,33 @@ async def on_ready():
     )
 
 @client.command(name='find', help='Gives an overview of the course requested')
-async def ind(ctx, course):
+async def find(ctx, course):
     response = scraper.course_info(course)
+    await ctx.send(response)
+
+@client.command(name='prereq', help='Gives prerequisites of course requested')
+async def prereq(ctx, course):
+    response = scraper.course_prereq(course)
+    await ctx.send(response)
+
+@client.command(name='name', help='Gives name of course requested')
+async def name(ctx, course):
+    response = scraper.course_name(course)
+    await ctx.send(response)
+
+@client.command(name='description', help='Gives description of course requested')
+async def description(ctx, course):
+    response = scraper.course_descrip(course)
+    await ctx.send(response)
+
+@client.command(name='breadth', help='Gives breadth requirements of course requested')
+async def breadth(ctx, course):
+    response = scraper.course_breadth(course)
+    await ctx.send(response)
+
+@client.command(name='exclusions', help='Gives exclusions of course requested')
+async def exclusion(ctx, course):
+    response = scraper.course_exclu(course)
     await ctx.send(response)
 
 client.run(TOKEN)
