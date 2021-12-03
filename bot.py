71413c752068865eb3f5f0a2f3436dfcfce9683f
gjchow <gabrieljchow@gmail.com>
2021-12-03 10:44:37 -0500

    Fixed scraper to work again and moved functions outside of scraper

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ import os
 import discord
 from dotenv import load_dotenv
 from discord.ext import commands
-import scraper
+import information
 
 load_dotenv(dotenv_path='.env')
 TOKEN = os.getenv('DISCORD_TOKEN')
@@ -24,32 +24,32 @@ async def on_ready():
 
 @client.command(name='find', help='Gives an overview of the course requested')
 async def find(ctx, course):
-    response = scraper.course_info(course)
+    response = information.course_info(course)
     await ctx.send(response)
 
 @client.command(name='prereq', help='Gives prerequisites of course requested')
 async def prereq(ctx, course):
-    response = scraper.course_prereq(course)
+    response = information.course_prereq(course)
     await ctx.send(response)
 
 @client.command(name='name', help='Gives name of course requested')
 async def name(ctx, course):
-    response = scraper.course_name(course)
+    response = information.course_name(course)
     await ctx.send(response)
 
 @client.command(name='description', help='Gives description of course requested')
 async def description(ctx, course):
-    response = scraper.course_descrip(course)
+    response = information.course_descrip(course)
     await ctx.send(response)
 
 @client.command(name='breadth', help='Gives breadth requirements of course requested')
 async def breadth(ctx, course):
-    response = scraper.course_breadth(course)
+    response = information.course_breadth(course)
     await ctx.send(response)
 
 @client.command(name='exclusions', help='Gives exclusions of course requested')
 async def exclusion(ctx, course):
-    response = scraper.course_exclu(course)
+    response = information.course_exclu(course)
     await ctx.send(response)
 
 client.run(TOKEN)
--- scraper.py
+++ scraper.py
@@ -1,107 +1,101 @@
 import requests
+import re
 from bs4 import BeautifulSoup
+from typing import List
 
 
-def scrape(course):
+def scrape(course: str) -> List[str]:
     out = []
-    URLy = "https://fas.calendar.utoronto.ca/course/" + course + "y1"
-    URLh = "https://fas.calendar.utoronto.ca/course/" + course + "h1"
-    pagey = requests.get(URLy)
-    pageh = requests.get(URLh)
+    course = course.lower()
+    utsg = re.compile(r'^\w{3}[1-4]\d{2}$')
+    utsc = re.compile(r'^\w{3}[A-Da-d]\d{2}$')
+    is_utsg = re.match(utsg, course)
+    is_utsc = re.match(utsc, course)
+    if is_utsg:
+        urly = "https://fas.calendar.utoronto.ca/course/" + course + "y1"
+        urlh = "https://fas.calendar.utoronto.ca/course/" + course + "h1"
+    elif is_utsc:
+        urly = "https://utsc.calendar.utoronto.ca/course/" + course + "y3"
+        urlh = "https://utsc.calendar.utoronto.ca/course/" + course + "h3"
+    else:
+        out.append('Not a valid course')
+        return out
+    pagey = requests.get(urly)
+    pageh = requests.get(urlh)
 
     soupy = BeautifulSoup(pagey.content, 'html.parser')
     souph = BeautifulSoup(pageh.content, 'html.parser')
 
-    namey = soupy.find(id='page-title')
-    nameh = souph.find(id='page-title')
-    if nameh.text.strip() != "Sorry, this course is not in the current Calendar.":
-        out.append(nameh.text.strip())
-        info = souph.find_all("div", class_="field-items")
-        labels = souph.find_all("div", class_="field-label")
-    elif namey.text.strip() != "Sorry, this course is not in the current Calendar.":
-        out.append(namey.text.strip())
-        info = soupy.find_all("div", class_="field-items")
-        labels = soupy.find_all("div", class_="field-label")
-    else:
+    namey = soupy.find(class_='page-title')
+    nameh = souph.find(class_='page-title')
+    namey = namey.text.strip()
+    nameh = nameh.text.strip()
+    if nameh != "Sorry, this course is not in the current Calendar." and \
+            nameh != "Sorry, this course has been retired and is no longer offered.":
+        out.append(nameh)
+        link = urlh
         info = []
-        labels = []
-
-    text_info = []
-    text_labels = []
-    for thing in info:
-        text_info.append(thing.text.strip())
-    for thing in labels:
-        text_labels.append(thing.text.strip())
-    if info != []:
-        description = text_info[1]
-        exclu_i = 0
-        exclu = "Exclusion:\nNone"
-        prereq_i = 0
-        prereq = "Prerequisite:\nNone"
-        coreq_i = 0
-        coreq = "Corequisite:\nNone"
-        if "Prerequisite:" in text_labels:
-            prereq_i = text_labels.index("Prerequisite:")
-        if "Exclusion:" in text_labels:
-            exclu_i = text_labels.index("Exclusion:")
-        breadth_i = text_labels.index("Breadth Requirements:")
-        if prereq_i != 0:
-            prereq = text_labels[prereq_i] + "\n" + text_info[prereq_i+1]
-        if exclu_i != 0:
-            exclu = text_labels[exclu_i] + "\n" + text_info[exclu_i+1]
-        breadth = text_labels[breadth_i] + "\n" + text_info[breadth_i+1]
-        if "Corequisite:" in text_labels:
-            coreq_i = text_labels.index("Corequisite:")
-        if coreq_i != 0:
-            coreq = text_labels[coreq_i] + "\n" + text_info[coreq_i+1]
-            prereq = coreq + "\n\n" + prereq
-
-        out.append(description)
-        out.append(prereq)
-        out.append(exclu)
-        out.append(breadth)
+        info.append(souph.find_all("div", class_="field--name-body")[3].text.strip())
+        for thing in souph.find_all("div", class_="field--name-field-prerequisite"):
+            find = thing.find(class_="field__item")
+            find = find.text.strip()
+            info.append(find)
+        for thing in souph.find_all("div", class_="field--name-field-corequisite"):
+            find = thing.find(class_="field__item")
+            find = find.text.strip()
+            info.append(find)
+        for thing in souph.find_all("div", class_="field--name-field-exclusion"):
+            find = thing.find(class_="field__item")
+            find = find.text.strip()
+            info.append(find)
+        for thing in souph.find_all("div", class_="field--name-field-breadth-requirements"):
+            find = thing.find(class_="field__item")
+            find = find.text.strip()
+            info.append(find)
+        labels = souph.find_all("div", class_="field__label")
+    elif namey != "Sorry, this course is not in the current Calendar." and \
+            namey != "Sorry, this course has been retired and is no longer offered.":
+        out.append(namey)
+        link = urly
+        info = soupy.find_all("div", class_="field__items")
+        labels = soupy.find_all("div", class_="field__label")
     else:
         return []
+    text_info = info
+    text_labels = []
+    for thing in labels:
+        text_labels.append(thing.text.strip())
+    if info == []:
+        return[]
 
+    if "Hours" in text_labels:
+        text_labels.remove("Hours")
+    description = text_info[0]
+    out.append(description)
+    if "Prerequisite" in text_labels:
+        prereq_i = text_labels.index("Prerequisite")
+        prereq = text_labels[prereq_i] + ": \n" + text_info[prereq_i + 1]
+        out.append(prereq)
+    else:
+        out.append(None)
+    if "Corequisite" in text_labels:
+        coreq_i = text_labels.index("Corequisite")
+        coreq = text_labels[coreq_i] + ": \n" + text_info[coreq_i + 1]
+        if out[2] is None:
+            out[2] = coreq
+        else:
+            out[2] = coreq + "\n\n" + out[2]
+    if "Exclusion" in text_labels:
+        exclu_i = text_labels.index("Exclusion")
+        exclu = text_labels[exclu_i] + ": \n" + text_info[exclu_i + 1]
+        out.append(exclu)
+    else:
+        out.append(None)
+    if "Breadth Requirements" in text_labels:
+        breadth_i = text_labels.index("Breadth Requirements")
+        breadth = text_labels[breadth_i] + ": \n" + text_info[breadth_i]
+        out.append(breadth)
+    else:
+        out.append(None)
+    out.append(link)
     return out
-
-def course_info(course):
-    if scrape(course) == []:
-        return 'Course not found'
-    out = ''
-    read = scrape(course)
-    for x in range(4):
-        out += read[x] + '\n\n'
-    out += read[4]
-    return out
-
-def course_name(course):
-    if scrape(course) == []:
-        return 'Course not found'
-    return scrape(course)[0]
-
-
-def course_descrip(course):
-    if scrape(course) == []:
-        return 'Course not found'
-    return scrape(course)[1]
-
-
-def course_prereq(course):
-    if scrape(course) == []:
-        return 'Course not found'
-    return scrape(course)[2]
-
-
-def course_exclu(course):
-    if scrape(course) == []:
-        return 'Course not found'
-    return scrape(course)[3]
-
-
-def course_breadth(course):
-    if scrape(course) == []:
-        return 'Course not found'
-    return scrape(course)[4]
-
-print(course_prereq("sta130"))
